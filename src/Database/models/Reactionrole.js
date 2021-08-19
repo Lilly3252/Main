ef2d2c81e5a4d712e4910b974aeb56b3f05c7604
Lillith3252 <74218901+Lilly3252@users.noreply.github.com>
2021-08-19 11:42:09 -0400

    Delete src directory

--- src/Database/models/Reactionrole.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const reactionSchema = mongoose.Schema({
-    Guild: String,
-    MessageID: String,
-    ReactionAndRole: Array,
-  });
-
-module.exports = mongoose.model('Reaction', reactionSchema, 'reactions');
--- src/Database/models/Wordfilter.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const wordfilterschema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  guildID: String,
-  Words: Map,
-});
